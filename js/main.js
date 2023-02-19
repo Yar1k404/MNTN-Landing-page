f383e60ec236a77a3490a30a5590317af961932a
Yar1k404 <moloko0107@gmail.com>
2023-02-20 00:26:18 +0200

    added adaptation and mobile navigation for landing page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const navBtn = document.querySelector('.nav-button')
+const mobileNav = document.querySelector('.mobile-nav')
+const body = document.body
+
+navBtn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    toggleMobileNav()
+})
+
+window.addEventListener('click', () => {
+    if (body.classList.contains('no-scroll')) {
+        toggleMobileNav()
+    }
+
+})
+
+mobileNav.addEventListener('click', (event) => {
+    event.stopPropagation()
+})
+
+const toggleMobileNav = () => {
+    mobileNav.classList.toggle('mobile-nav-active')
+    navBtn.classList.toggle('nav-button-close')
+    body.classList.toggle('no-scroll')
+}
